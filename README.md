64aea80410ba2c89ec491be49cf7af598a8e3ca3
Berkay Gediz <121058050+berkaygediz@users.noreply.github.com>
2023-09-04 05:39:24 +0300

    updated code and readme.

--- README.md
+++ README.md
@@ -1,19 +1,42 @@
-# JSON Market
+# E-Commerce Application
 
-## Requirements
+This example represents a JavaScript-based application that simulates an e-commerce platform. The application manages products, customers, and orders, and calculates the total amount for each order, including KDV (Value Added Tax).
 
-- Node.js 12.16.1 or higher
+## Table of Contents
 
-## Usage
+- [Introduction](#introduction)
+- [Data Structure](#data-structure)
+- [Calculating Total Amounts](#calculating-total-amounts)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
 
-```
-node script.js
-```
+## Introduction
 
-## Contributing
+This code example demonstrates the structure of an e-commerce application with JavaScript. It contains data for products, customers, and orders, and it calculates the total amount paid for each order, considering a KDV rate of 20%.
 
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+## Data Structure
 
-## License
+- **Products:** An array containing product information, including ID, title, URL, and price.
 
-This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
+- **Customers:** An array storing customer data, including ID, name, surname, address, district, and city.
+
+- **Sellers:** An array representing seller companies with their respective IDs.
+
+- **Orders:** An array of orders with details such as customer, seller, date, payment method, shipping address, purchased items, and total price.
+
+## Calculating Total Amounts
+
+The code calculates the total amount paid for each order, including KDV. It then displays the results in the console.
+
+### Usage
+
+To use this code, you can copy it into your JavaScript environment and run it. It will display the total amount paid, including KDV, for each order, as well as the total amount for all orders.
+
+### Contributing
+
+Contributions to this code example are welcome! Feel free to fork this repository, make improvements, and create a pull request.
+
+### License
+
+This code is licensed under the [Apache License 2.0](LICENSE).
--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let products = [
   {
     id: 5,
-    title: "iphone 13 pro",
+    title: "iPhone 13 Pro",
     url: "https://abc.com/iphone-13-pro",
     price: 22000,
   },
   {
     id: 6,
-    title: "iphone 13 pro max",
+    title: "iPhone 13 Pro Max",
     url: "https://abc.com/iphone-13-pro-max",
     price: 25000,
   },
-  { id: 7, title: "iphone x", url: "https://abc.com/iphone-x", price: 10000 },
-  { id: 8, title: "iphone se", url: "https://abc.com/iphone-se", price: 8000 },
+  { id: 7, title: "iPhone X", url: "https://abc.com/iphone-x", price: 10000 },
+  { id: 8, title: "iPhone SE", url: "https://abc.com/iphone-se", price: 8000 },
 ];
 
 let customers = [
@@ -65,13 +65,8 @@ let orders = [
     customer: customers[0].id,
     seller: sellers[0].id,
     date: (orderDate = new Date(2023, 4, 11)),
-    payment: "credit card",
-    shipping_address:
-      customers[0].address +
-      " " +
-      customers[0].district +
-      " " +
-      customers[0].city,
+    payment: "Credit Card",
+    shipping_address: `${customers[0].address} ${customers[0].district} ${customers[0].city}`,
     purchased_items: [products[0].id, products[3].id],
     total_price: [products[0].price, products[3].price],
   },
@@ -80,13 +75,8 @@ let orders = [
     customer: customers[1].id,
     seller: sellers[1].id,
     date: (orderDate = new Date(2023, 4, 12)),
-    payment: "prepaid",
-    shipping_address:
-      customers[1].address +
-      " " +
-      customers[1].district +
-      " " +
-      customers[1].city,
+    payment: "Prepaid",
+    shipping_address: `${customers[1].address} ${customers[1].district} ${customers[1].city}`,
     purchased_items: [products[1].id, products[2].id],
     total_price: [products[1].price, products[2].price],
   },
@@ -95,13 +85,8 @@ let orders = [
     customer: customers[2].id,
     seller: sellers[2].id,
     date: (orderDate = new Date(2023, 4, 13)),
-    payment: "cash on delivery",
-    shipping_address:
-      customers[2].address +
-      " " +
-      customers[2].district +
-      " " +
-      customers[2].city,
+    payment: "Cash on Delivery",
+    shipping_address: `${customers[2].address} ${customers[2].district} ${customers[2].city}`,
     purchased_items: [products[0].id, products[1].id],
     total_price: [products[0].price, products[1].price],
   },
@@ -110,42 +95,36 @@ let orders = [
     customer: customers[3].id,
     seller: sellers[3].id,
     date: (orderDate = new Date(2023, 4, 14)),
-    payment: "credit card",
-    shipping_address:
-      customers[3].address +
-      " " +
-      customers[3].district +
-      " " +
-      customers[3].city,
+    payment: "Credit Card",
+    shipping_address: `${customers[3].address} ${customers[3].district} ${customers[3].city}`,
     purchased_items: [products[1].id, products[3].id],
     total_price: [products[1].price, products[3].price],
   },
 ];
 
-let kdv = 1.18;
-let totalAmounts = new Array();
-let orderIds = new Array();
+let kdv = 1.20;
+let totalAmounts = [];
+let orderIds = [];
 
-console.log("-------Total Amount Paid Including KDV for Each Order-------");
+console.log("------- Total Amount Paid Including KDV for Each Order -------");
 for (let i = 0; i < orders.length; i++) {
   let totalPrice = 0;
 
   for (let j = 0; j < orders[i].total_price.length; j++) {
     totalPrice += orders[i].total_price[j];
   }
-  let totalWithVAT = totalPrice * kdv;
+  let totalWithKDV = totalPrice * kdv;
   console.log(
-    "Total amount paid for order with ID " + orders[i].id + " = " + totalWithVAT
+    `Order ID: ${orders[i].id}, Total Amount Paid (with KDV): ${totalWithKDV}`
   );
-
-  totalAmounts[i] = totalWithVAT;
+  totalAmounts[i] = totalWithKDV;
   orderIds[i] = orders[i].id;
 }
 
-console.log("\n-------Total Amount Paid Including KDV for All Orders-------");
-console.log("Order IDs: " + orderIds);
+console.log("\n------- Total Amount Paid Including KDV for All Orders -------");
+console.log(`Order IDs: ${orderIds}`);
 let totalAmount = 0;
 for (let x = 0; x < totalAmounts.length; x++) {
   totalAmount += totalAmounts[x];
 }
-console.log("Total amount paid for all orders = " + totalAmount);
+console.log(`Total amount paid for all orders = ${totalAmount}`);
